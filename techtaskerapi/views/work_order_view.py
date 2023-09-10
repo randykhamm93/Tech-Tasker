ca19e49f8d6f295cc28147a18c7e23de78abce6f
Randy Hamm <randykhamm@gmail.com>
2023-09-10 15:12:15 -0500

    added create method to WorkOrderView class

--- techtaskerapi/views/work_order_view.py
+++ techtaskerapi/views/work_order_view.py
@@ -31,6 +31,28 @@ class WorkOrderView(ViewSet):
         serialized = WorkOrderSerializer(work_order, context={'request': request})
         return Response(serialized.data, status=status.HTTP_200_OK)
 
+    def create(self, request):
+        """Handle POST requests for service tickets
+
+        Returns:
+            Response: JSON serialized representation of newly created service ticket
+        """
+        new_work_order = WorkOrder()
+        new_work_order.title = request.data.get('title', '')  # Set the title field
+        new_work_order.description = request.data['description']
+        new_work_order.critical = request.data.get('critical', False)  # Set the critical field, defaulting to False
+        new_work_order.status = request.data.get('status', WorkOrder.NOT_STARTED)  # Set the status field, defaulting to 'Not Started'
+        new_work_order.due_date = request.data.get('due_date', '')  # Set the due_date field
+        new_work_order.created_by_user = request.user  # Set the created_by_user field
+        new_work_order.department_id = request.data.get('department', '')  # Set the department field
+        new_work_order.category_id = request.data.get('category', '')  # Set the category field
+
+        new_work_order.save()
+
+        serialized = WorkOrderSerializer(new_work_order, many=False)
+
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+
 
 class WorkOrderSerializer(serializers.ModelSerializer):
     """JSON serializer for work orders"""
